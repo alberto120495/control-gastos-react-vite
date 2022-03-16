e709acf4c87866ae252f0f5d1dee3df111370c70
Alberto <albertopimentel04@gmail.com>
2022-03-16 17:22:05 -0600

    modal y formulario completo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
 import Header from "./components/Header";
+import Modal from "./components/Modal";
+import { generarId } from "./helpers";
 import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+
+  const [gastos, setGastos] = useState([]);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
+  };
+
+  const guardarGasto = (gasto) => {
+    gasto.id = generarId();
+    setGastos([...gastos, gasto]);
+  };
   return (
     <>
       <Header
@@ -17,8 +36,21 @@ function App() {
 
       {isValidPresupuesto && (
         <div className="nuevo-gasto">
-          <img src={IconoNuevoGasto} alt="icono nuevo gasto" />
+          <img
+            src={IconoNuevoGasto}
+            alt="icono nuevo gasto"
+            onClick={handleNuevoGasto}
+          />
         </div>
+      )}
+
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
+        />
       )}
     </>
   );
