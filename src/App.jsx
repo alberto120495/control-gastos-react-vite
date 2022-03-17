77dd6407f35286063a9124bc3a02ccebe7f4a826
Alberto <albertopimentel04@gmail.com>
2022-03-17 16:19:10 -0600

    filtro completo y estilo negativo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import Filtros from "./components/Filtros";
 import Header from "./components/Header";
 import ListadoGastos from "./components/ListadoGastos";
 import Modal from "./components/Modal";
@@ -23,6 +24,10 @@ function App() {
 
   const [gastoEditar, setGastoEditar] = useState({});
 
+  const [filtro, setFiltro] = useState("");
+
+  const [gastosFiltrados, setGastosFiltrados] = useState([]);
+
   useEffect(() => {
     if (Object.keys(gastoEditar).length > 0) {
       setModal(true);
@@ -42,17 +47,17 @@ function App() {
   }, [gastos]);
 
   useEffect(() => {
+    if (filtro) {
+      //?Filtrar gastos
+      const gastosFilter = gastos.filter((gasto) => gasto.categoria === filtro);
+      setGastosFiltrados(gastosFilter);
+    }
+  }, [filtro]);
+
+  useEffect(() => {
     const presupuestoLS = Number(localStorage.getItem("presupuesto"));
     if (presupuestoLS > 0) {
       setIsValidPresupuesto(true);
-    }
-  }, []);
-
-  useEffect(() => {
-    const gastosLS = JSON.parse(localStorage.getItem("gastos"));
-
-    if (gastosLS.length) {
-      console.log(gastos);
     }
   }, []);
 
@@ -102,10 +107,13 @@ function App() {
       {isValidPresupuesto && (
         <>
           <main>
+            <Filtros filtro={filtro} setFiltro={setFiltro} />
             <ListadoGastos
               gastos={gastos}
               setGastoEditar={setGastoEditar}
               eliminarGasto={eliminarGasto}
+              filtro={filtro}
+              gastosFiltrados={gastosFiltrados}
             />
           </main>
           <div className="nuevo-gasto">
--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -41,9 +41,9 @@ function ControlPresupuesto({ presupuesto, gastos }) {
           text={`${porcentaje}% Gastado`}
           value={porcentaje}
           styles={buildStyles({
-            pathColor: "#3B82F6",
+            pathColor: porcentaje > 100 ? "#dc2626" : "#3B82F6",
             trailColor: "#f5f5f5",
-            textColor: "#3B82F6",
+            textColor: porcentaje > 100 ? "#dc2626" : "#3B82F6",
           })}
         />
       </div>
@@ -53,7 +53,7 @@ function ControlPresupuesto({ presupuesto, gastos }) {
           <span>Presupuesto: </span>
           {formatearCantidad(presupuesto)}
         </p>
-        <p>
+        <p className={`${disponible < 0 ? "negativo" : ""}`}>
           <span>Disponible: </span>
           {formatearCantidad(disponible)}
         </p>
